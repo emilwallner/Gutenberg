0101c21e9d6e889fa842643dd32119bd85771b50
Emil Wallner <emilw@llner.co>
2017-01-04 23:57:48 +0100

    update

---
+++ gcc.sh
@@ -0,0 +1,1 @@
+gcc -Wextra -Wall -Werror main.c -I ft_print.h ./libft/libft.a && ./a.out
--- main.c
+++ main.c
@@ -6,15 +6,45 @@
 /*   By: ewallner <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/08 08:23:12 by ewallner          #+#    #+#             */
-/*   Updated: 2016/12/08 12:36:04 by ewallner         ###   ########.fr       */
+/*   Updated: 2017/01/04 22:54:03 by ewallner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
+#include <stdarg.h>
+#include "./libft/libft.h"
 
-int			main(void)
+int ft_printf(char *str, ...)
 {
-	
-	printf("%ld", 15);
-	return (0);
+	int val = 0;
+	va_list ap;
+	int i;
+
+	i = -1;
+	va_start(ap, str);
+	while(str[++i])
+	{
+		if(str[i] == '%' && str[i + 1] == 'd')
+		{
+			ft_putnbr(va_arg(ap, int));
+			i++;
+		}
+		else if(str[i] == '%' && str[i + 1] == 's')
+		{
+			ft_putstr(va_arg(ap, char *));
+			i++;
+		}
+		else
+			ft_putchar(str[i]);
+	}
+	va_end(ap);
+
+	return val;
+}
+
+
+int main(void)
+{
+	ft_printf("This is a string: %s \nThis is a number: %d \n", "It's workin", 42);
+	return 0;
 }
