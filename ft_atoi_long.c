01aeb7f43e76fbe5c918ae6717b3680a0d699215
Emil Wallner <emilw@llner.co>
2017-02-10 19:25:57 +0100

    norm clean

--- ft_atoi_long.c
+++ ft_atoi_long.c
@@ -6,17 +6,17 @@
 /*   By: ewallner <ewallner@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/07 16:35:59 by ewallner          #+#    #+#             */
-/*   Updated: 2017/02/10 17:10:47 by ewallner         ###   ########.fr       */
+/*   Updated: 2017/02/10 19:25:33 by ewallner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-long long		ft_atoi_long(const char *s)
+long long	ft_atoi_long(const char *s)
 {
 	int					i;
 	int					isneg;
-	long long		sum;
+	long long			sum;
 
 	sum = 0;
 	i = 0;
@@ -34,8 +34,8 @@ long long		ft_atoi_long(const char *s)
 		sum = sum + (s[i] - '0');
 		i++;
 	}
-	if(i > 19)
-		return(2147483649);
+	if (i > 19)
+		return (2147483649);
 	else
 		return (sum * isneg);
 }
