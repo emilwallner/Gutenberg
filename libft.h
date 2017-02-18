cf2fd178f1b8efe050cd7a57d5c464d43ebd67e7
Emil Wallner <emilw@llner.co>
2017-02-18 18:40:59 +0100

    Color

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: ewallner <ewallner@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/04 20:13:07 by ewallner          #+#    #+#             */
-/*   Updated: 2017/02/12 15:03:33 by ewallner         ###   ########.fr       */
+/*   Updated: 2017/02/18 17:55:18 by ewallner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,13 @@
 # define BUFF_SIZE 10
 # define BASE16 "0123456789ABCDEF"
 # define BASE2	"01"
+# define ANSI_COLOR_RED     "\x1b[31m"
+# define ANSI_COLOR_GREEN   "\x1b[32m"
+# define ANSI_COLOR_YELLOW  "\x1b[33m"
+# define ANSI_COLOR_BLUE    "\x1b[34m"
+# define ANSI_COLOR_MAGENTA "\x1b[35m"
+# define ANSI_COLOR_CYAN    "\x1b[36m"
+# define ANSI_COLOR_RESET   "\x1b[0m"
 
 void		ft_base(unsigned int i, unsigned int base_length, char *base);
 int			ft_strstrlen(char **str);
